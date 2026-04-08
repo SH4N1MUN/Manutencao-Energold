ca552402aa7dace55c8cb15c3363e7b34b526a89
SH4N1MUN <financeironc.gabriel@gmail.com>
2026-04-08 15:12:51 -0300

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "COLE_SUA_API_KEY",
+  authDomain: "SEU-PROJETO.firebaseapp.com",
+  projectId: "SEU-PROJETO",
+  storageBucket: "SEU-PROJETO.firebasestorage.app",
+  messagingSenderId: "000000000000",
+  appId: "1:000000000000:web:SEUAPPID"
+};
