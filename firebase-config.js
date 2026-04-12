fa38e76202679b078e6329e5a38f6968f34c4847
SH4N1MUN <financeironc.gabriel@gmail.com>
2026-04-12 10:03:43 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Edite este arquivo com as credenciais do seu projeto Firebase.
-export const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY",
-  authDomain: "SEU-PROJETO.firebaseapp.com",
-  projectId: "SEU-PROJETO",
-  storageBucket: "SEU-PROJETO.firebasestorage.app",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:SEUAPPID"
-};
