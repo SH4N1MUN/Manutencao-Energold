df53706bb200afe8e2c8027cf10c09be5433f480
SH4N1MUN <financeironc.gabriel@gmail.com>
2026-04-13 19:30:13 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  APP_NAME: 'ENERGOLD - Controle de Manutenção',
-  VERSION: 'github-pwa-v1',
-  REPOSITORY_STRUCTURE: 'modular',
-  GENERATED_AT: new Date().toISOString()
-};
